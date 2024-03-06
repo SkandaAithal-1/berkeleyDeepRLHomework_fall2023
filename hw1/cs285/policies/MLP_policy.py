f9ee8fd7c5e3be2586c477ddc2f6c6e7c7605813
Skanda <SkandaAithal-1>
2024-03-06 20:38:14 +0530

    Solutions added

--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -9,7 +9,7 @@ Functions to edit:
 import abc
 import itertools
 from typing import Any
-from torch import nn
+from torch import from_numpy, nn
 from torch.nn import functional as F
 from torch import optim
 
@@ -129,7 +129,8 @@ class MLPPolicySL(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
         # through it. For example, you can return a torch.FloatTensor. You can also
         # return more flexible objects, such as a
         # `torch.distributions.Distribution` object. It's up to you!
-        raise NotImplementedError
+        action = self.mean_net(observation)
+        return action
 
     def update(self, observations, actions):
         """
@@ -141,7 +142,12 @@ class MLPPolicySL(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             dict: 'Training Loss': supervised learning loss
         """
         # TODO: update the policy and return the loss
-        loss = TODO
+        lossFn = nn.MSELoss()
+        self.optimizer.zero_grad()
+        acPred = self(ptu.from_numpy(observations))
+        loss = lossFn(acPred, from_numpy(actions))
+        loss.backward()
+        self.optimizer.step()
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
--- hw1/cs285/scripts/run_hw1.py
+++ hw1/cs285/scripts/run_hw1.py
@@ -132,8 +132,7 @@ def run_training_loop(params):
             # TODO: collect `params['batch_size']` transitions
             # HINT: use utils.sample_trajectories
             # TODO: implement missing parts of utils.sample_trajectory
-            paths, envsteps_this_batch = TODO
-
+            paths, envsteps_this_batch = utils.sample_trajectories(env, actor, params['batch_size'], params['ep_len'])
             # relabel the collected obs with actions from a provided expert policy
             if params['do_dagger']:
                 print("\nRelabelling collected observations with labels from an expert policy...")
@@ -141,7 +140,8 @@ def run_training_loop(params):
                 # TODO: relabel collected obsevations (from our policy) with labels from expert policy
                 # HINT: query the policy (using the get_action function) with paths[i]["observation"]
                 # and replace paths[i]["action"] with these expert labels
-                paths = TODO
+                for path in paths:
+                    path['action'] = expert_policy.get_action(path['observation'])
 
         total_envsteps += envsteps_this_batch
         # add collected data to replay buffer
@@ -157,11 +157,12 @@ def run_training_loop(params):
           # HINT2: use np.random.permutation to sample random indices
           # HINT3: return corresponding data points from each array (i.e., not different indices from each array)
           # for imitation learning, we only need observations and actions.  
-          ob_batch, ac_batch = TODO
+            randIndices = np.random.permutation(min(len(replay_buffer), params['train_batch_size']))
+            ob_batch, ac_batch = replay_buffer.obs[randIndices[:params['train_batch_size']]], replay_buffer.acs[randIndices[:params['train_batch_size']]]
 
           # use the sampled data to train an agent
-          train_log = actor.update(ob_batch, ac_batch)
-          training_logs.append(train_log)
+            train_log = actor.update(ob_batch, ac_batch)
+            training_logs.append(train_log)
 
         # log/save
         print('\nBeginning logging procedure...')
@@ -173,6 +174,7 @@ def run_training_loop(params):
 
             # save videos
             if eval_video_paths is not None:
+                print(f"Logged the video dude")
                 logger.log_paths_as_videos(
                     eval_video_paths, itr,
                     fps=fps,
